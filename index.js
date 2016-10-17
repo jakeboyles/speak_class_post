035392f2030f6d8737e1542b29483048d8f54132
Jake Boyles <jake@jibdesigns.com>
2016-10-17 12:25:17 -0400

    adding in slack, twillio etc

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const say = require('say');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const client = require('twilio')('AC645a219ee3e2fb800e62b262ecdaada4', '#');
+const Slack = require('node-slack');
+const slack = new Slack("https://hooks.slack.com/services/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");
 
 app.use(cors());
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -10,8 +13,21 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.post('/talkToMe',(req, res) => {
-    say.speak(`${req.body.name} says ${req.body.text}`);
+app.post('/doSomething',(req, res) => {
+	var string = `${req.body.name} is going to drink some ${req.body.restaurant} for ${req.body.meal}`;
+    
+    say.speak(string);
+
+    client.sendMessage({
+        to:'+15135932378', 
+        from: '+15132808426', 
+        body: string,
+      });
+    
+    slack.send({
+        text: string,
+      });
+
     res.json({msg: 'Thanks for talking to me!'});
 });
 
